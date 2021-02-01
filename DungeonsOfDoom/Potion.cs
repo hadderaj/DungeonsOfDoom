b68cd5d1da932cae8048f17655c1895db3125dda
hadderaj <77488231+hadderaj@users.noreply.github.com>
2021-02-01 16:51:06 +0100

    Add project files.

---
+++ DungeonsOfDoom/Potion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonsOfDoom
+{
+    class Potion : Item
+    {
+        public Potion(string name, int healthMod) : base(name)
+        {
+            HealthMod = healthMod;
+        }
+        public int HealthMod { get; set; }
+    }
+
+}
---
+++ DungeonsOfDoom/Weapon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonsOfDoom
+{
+    class Weapon : Item
+    {
+        public Weapon(string name, int dmgMod) : base(name)
+        {
+            DmgMod = dmgMod;
+        }
+        public int DmgMod { get; set; }
+    }
+}
